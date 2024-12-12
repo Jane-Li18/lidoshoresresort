54ba38d996abfbaf2e9314cafc6a82906c8b4faa
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2024-12-12 20:31:40 +0800

    wsgi

--- lidoshoresresort/settings.py
+++ lidoshoresresort/settings.py
@@ -102,6 +102,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lidoshoresresort.wsgi.application'
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
